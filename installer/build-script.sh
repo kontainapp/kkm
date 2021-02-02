9434149cd4f683a8788891c762ee1a4c50c31a84
Srinivasa Vetsa <svetsa@kontain.app>
2021-02-02 11:42:11 -0800

    identify installation tool.

--- installer/build-script.sh
+++ installer/build-script.sh
@@ -1,44 +1,43 @@
 #!/usr/bin/sh
 
-# make sure kitchen sink is available
+MAJOR_VERSION=`uname -r | cut -d'.' -f1-1`
+MINOR_VERSION=`uname -r | cut -d'.' -f2-2`
+
+# support only kernel 5+
+if [ $MAJOR_VERSION -lt 5 ]; then
+	echo "KKM is only supported above 5.x kernel"
+	exit 1
+fi
+
+# if we have dnf or apt we can use it to install other wise bailout.
+if [ -f /usr/bin/dnf ]; then
+	PACKAGE_LIST="kmod patch bash tar git-core bzip2 xz findutils gzip m4 perl-interpreter perl-Carp perl-devel perl-generators make diffutils gawk gcc binutils redhat-rpm-config hmaccalc bison flex net-tools hostname bc elfutils-devel dwarves python3-devel rsync xmlto asciidoc python3-sphinx sparse zlib-devel binutils-devel newt-devel bison flex xz-devel gettext ncurses-devel pciutils-devel zlib-devel binutils-devel clang llvm numactl-devel libcap-devel libcap-ng-devel rsync rpm-build elfutils kabi-dw openssl openssl-devel nss-tools xmlto asciidoc"
+	INSTALLER_CMD="/usr/bin/dnf install -y ${PACKAGE_LIST}"
+elif [ -f /usr/bin/apt ]; then
+	INSTALLER_CMD="/usr/bin/apt install -y "
+else
+	echo "Cannot find dnf or apt-get exiting"
+	exit 1
+fi
+
+# make sure kitchen sink is available on fedora
 sudo -u root /usr/bin/bash << INSTALL_END
 
-dnf install -y kmod patch bash tar git-core
-dnf install -y bzip2 xz findutils gzip m4 perl-interpreter perl-Carp perl-devel perl-generators make diffutils gawk
-dnf install -y gcc binutils redhat-rpm-config hmaccalc bison flex
-dnf install -y net-tools hostname bc elfutils-devel
-dnf install -y dwarves
-dnf install -y python3-devel
-dnf install -y rsync
-dnf install -y xmlto asciidoc python3-sphinx
-dnf install -y sparse
-dnf install -y zlib-devel binutils-devel newt-devel bison flex xz-devel
-dnf install -y gettext ncurses-devel
-dnf install -y pciutils-devel
-dnf install -y zlib-devel binutils-devel
-dnf install -y clang llvm
-dnf install -y numactl-devel
-dnf install -y libcap-devel libcap-ng-devel rsync
-dnf install -y rpm-build elfutils
-dnf install -y kabi-dw
-dnf install -y openssl openssl-devel
-dnf install -y nss-tools
-dnf install -y xmlto
-dnf install -y asciidoc
+${INSTALLER_CMD}
 
 INSTALL_END
 
-pwd
-ls -l
+# script is running in root directory of extracted files
 
-echo "running kkm build script"
+echo "Building kkm.ko"
 make -C kkm
 
-echo "installing kkm.ko"
+echo "Installing kkm.ko"
 sudo make -C kkm modules_install
 
-# Fedora
-sudo cp installer/fedora/modprobe.d/kkm.conf /etc/modprobe.d/kkm.conf
-sudo cp installer/fedora/modules-load.d/kkm.conf /etc/modules-load.d/kkm.conf
+# files required for kkm module in /etc/
+echo "Installing /etc file for kkm"
+sudo cp installer/etc/modprobe.d/kkm.conf /etc/modprobe.d/kkm.conf
+sudo cp installer/etc/modules-load.d/kkm.conf /etc/modules-load.d/kkm.conf
 
 echo "done"
