42afc29b31174914197635bdd767fc0ad73ba9b8
Srinivasa Vetsa <svetsa@kontain.app>
2021-02-02 18:02:43 -0800

    both fedora and ubuntu work

--- installer/build-script.sh
+++ installer/build-script.sh
@@ -11,10 +11,11 @@ fi
 
 # if we have dnf or apt we can use it to install other wise bailout.
 if [ -f /usr/bin/dnf ]; then
-	PACKAGE_LIST="kmod patch bash tar git-core bzip2 xz findutils gzip m4 perl-interpreter perl-Carp perl-devel perl-generators make diffutils gawk gcc binutils redhat-rpm-config hmaccalc bison flex net-tools hostname bc elfutils-devel dwarves python3-devel rsync xmlto asciidoc python3-sphinx sparse zlib-devel binutils-devel newt-devel bison flex xz-devel gettext ncurses-devel pciutils-devel zlib-devel binutils-devel clang llvm numactl-devel libcap-devel libcap-ng-devel rsync rpm-build elfutils kabi-dw openssl openssl-devel nss-tools xmlto asciidoc"
+	PACKAGE_LIST=" kmod patch bash tar git-core bzip2 xz findutils gzip m4 perl-interpreter perl-Carp perl-devel perl-generators make diffutils gawk gcc binutils redhat-rpm-config hmaccalc bison flex net-tools hostname bc elfutils-devel dwarves python3-devel rsync xmlto asciidoc python3-sphinx sparse zlib-devel binutils-devel newt-devel bison flex xz-devel gettext ncurses-devel pciutils-devel zlib-devel binutils-devel clang llvm numactl-devel libcap-devel libcap-ng-devel rsync rpm-build elfutils kabi-dw openssl openssl-devel nss-tools xmlto asciidoc"
 	INSTALLER_CMD="/usr/bin/dnf install -y ${PACKAGE_LIST}"
 elif [ -f /usr/bin/apt ]; then
-	INSTALLER_CMD="/usr/bin/apt install -y "
+	PACKAGE_LIST=" make gcc "
+	INSTALLER_CMD="/usr/bin/apt install -y ${PACKAGE_LIST}"
 else
 	echo "Cannot find dnf or apt-get exiting"
 	exit 1
