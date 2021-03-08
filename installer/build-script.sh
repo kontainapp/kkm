0b07b813488b7dfb9c97a358935f90cc613f6534
Srinivasa Vetsa <svetsa@kontain.app>
2021-03-08 15:07:25 -0800

    licensing

--- installer/build-script.sh
+++ installer/build-script.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/sh
+# Copyright © 2020-2019 Kontain Inc. All rights reserved.
+#
+# Kontain Inc CONFIDENTIAL
+#
+#  This file includes unpublished proprietary source code of Kontain Inc. The
+#  copyright notice above does not evidence any actual or intended publication of
+#  such source code. Disclosure of this source code or any related proprietary
+#  information is strictly prohibited without the express written permission of
+#  Kontain Inc.
 
 MAJOR_VERSION=`uname -r | cut -d'.' -f1-1`
 MINOR_VERSION=`uname -r | cut -d'.' -f2-2`
