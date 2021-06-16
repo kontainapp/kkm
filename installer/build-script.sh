c3d44ef91e70e6bb5f54a01c4cb9436e368f0e0a
Srinivasa Vetsa <svetsa@kontain.app>
2021-06-16 15:27:25 -0700

    basic dkms

--- installer/build-script.sh
+++ installer/build-script.sh
@@ -20,10 +20,10 @@ fi
 
 # if we have dnf or apt we can use it to install other wise bailout.
 if [ -f /usr/bin/dnf ]; then
-	PACKAGE_LIST=" kmod patch bash tar git-core bzip2 xz findutils gzip m4 perl-interpreter perl-Carp perl-devel perl-generators make diffutils gawk gcc binutils redhat-rpm-config hmaccalc bison flex net-tools hostname bc elfutils-devel dwarves python3-devel rsync xmlto asciidoc python3-sphinx sparse zlib-devel binutils-devel newt-devel bison flex xz-devel gettext ncurses-devel pciutils-devel zlib-devel binutils-devel clang llvm numactl-devel libcap-devel libcap-ng-devel rsync rpm-build elfutils kabi-dw openssl openssl-devel nss-tools xmlto asciidoc"
+	PACKAGE_LIST=" kmod patch bash tar git-core bzip2 xz findutils gzip m4 perl-interpreter perl-Carp perl-devel perl-generators make diffutils gawk gcc binutils redhat-rpm-config hmaccalc bison flex net-tools hostname bc elfutils-devel dwarves python3-devel rsync xmlto asciidoc python3-sphinx sparse zlib-devel binutils-devel newt-devel bison flex xz-devel gettext ncurses-devel pciutils-devel zlib-devel binutils-devel clang llvm numactl-devel libcap-devel libcap-ng-devel rsync rpm-build elfutils kabi-dw openssl openssl-devel nss-tools xmlto asciidoc dkms "
 	INSTALLER_CMD="/usr/bin/dnf install -y ${PACKAGE_LIST}"
 elif [ -f /usr/bin/apt ]; then
-	PACKAGE_LIST=" make gcc "
+	PACKAGE_LIST=" make gcc dkms build-essential "
 	INSTALLER_CMD="/usr/bin/apt update; /usr/bin/apt install -y ${PACKAGE_LIST}"
 else
 	echo "Cannot find dnf or apt exiting"
@@ -39,21 +39,26 @@ INSTALL_END
 
 # script is running in root directory of extracted files
 
-echo "Building kkm.ko"
-make -C kkm
+# setup kkm
 
-echo "Installing kkm.ko"
-sudo make -C kkm modules_install
+# remove
+sudo dkms remove -q -m kkm -v 0.9 --all
+sudo rm -fr /usr/src/kkm-0.9
+rm -f /var/lib/dkms/kkm
 
-# some of the installs have incorrect /boot links, run depmod for running kernel
-sudo depmod -a `uname -r`
-
-# files required for kkm module in /etc/
-echo "Installing /etc file for kkm"
+umask 022
 sudo cp installer/etc/modprobe.d/kkm.conf /etc/modprobe.d/kkm.conf
 sudo cp installer/etc/modules-load.d/kkm.conf /etc/modules-load.d/kkm.conf
 
-echo "Doing modprobe kkm"
+# add
+sudo mkdir -p /usr/src/kkm-0.9
+sudo cp -r kkm /usr/src/kkm-0.9
+sudo cp -r licenses /usr/src/kkm-0.9
+sudo cp installer/dkms.conf /usr/src/kkm-0.9
+sudo dkms add -c /usr/src/kkm-0.9/dkms.conf -m kkm -v 0.9
+sudo dkms build -m kkm -v 0.9
+sudo dkms install -m kkm -v 0.9
+
 sudo modprobe kkm
 
 echo "done"
