3b28b659333c23bca944a6cbf712163c1c2af20f
Serge Pashenkov <serge@kontain.app>
2021-07-22 16:01:31 -0700

    squelch progrss bar in install script, for packer

--- installer/build-script.sh
+++ installer/build-script.sh
@@ -54,7 +54,7 @@ if [ -f /usr/bin/dnf ]; then
 	INSTALLER_CMD="/usr/bin/dnf install -y ${PACKAGE_LIST}"
 elif [ -f /usr/bin/apt ]; then
 	PACKAGE_LIST=" make gcc dkms build-essential "
-	INSTALLER_CMD="/usr/bin/apt update; /usr/bin/apt install -y ${PACKAGE_LIST}"
+	INSTALLER_CMD="/usr/bin/apt-get -q -y update; /usr/bin/apt-get -q -y install -y ${PACKAGE_LIST}"
 else
 	echo "Cannot find dnf or apt exiting"
 	exit 1
