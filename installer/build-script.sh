0c28855511f4357d6c074f6bdcee8f7876cba2c6
John Muth <muth@kontain.app>
2022-01-17 17:21:04 -0800

    add yum support to build-script.sh (#6)

--- installer/build-script.sh
+++ installer/build-script.sh
@@ -52,13 +52,16 @@ if [ $? -ne 0 ]; then
 	fi
 fi
 
-# if we have dnf or apt we can use it to install other wise bailout.
+# if we have dnf or apt or yum we can use it to install other wise bailout.
 if [ -f /usr/bin/dnf ]; then
 	PACKAGE_LIST=" kmod patch bash tar git-core bzip2 xz findutils gzip m4 perl-interpreter perl-Carp perl-devel perl-generators make diffutils gawk gcc binutils redhat-rpm-config hmaccalc bison flex net-tools hostname bc elfutils-devel dwarves python3-devel rsync xmlto asciidoc python3-sphinx sparse zlib-devel binutils-devel newt-devel bison flex xz-devel gettext ncurses-devel pciutils-devel zlib-devel binutils-devel clang llvm numactl-devel libcap-devel libcap-ng-devel rsync rpm-build elfutils kabi-dw openssl openssl-devel nss-tools xmlto asciidoc dkms "
 	INSTALLER_CMD="/usr/bin/dnf install -y ${PACKAGE_LIST}"
 elif [ -f /usr/bin/apt ]; then
 	PACKAGE_LIST=" make gcc dkms build-essential "
 	INSTALLER_CMD="/usr/bin/apt-get -q -y update; /usr/bin/apt-get -q -y install -y ${PACKAGE_LIST}"
+elif [ -f /usr/bin/yum ]; then
+	PACKAGE_LIST=" make gcc dkms build-essential "
+	INSTALLER_CMD="/usr/bin/yum -q -y update; /usr/bin/yum -q -y install -y ${PACKAGE_LIST}"
 else
 	echo "Cannot find dnf or apt exiting"
 	exit 1
